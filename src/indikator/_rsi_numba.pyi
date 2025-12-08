96f9ddab5d9184caff7acf021726bac5cd7ed19a
Sencer S. <sencer@nospam.com>
2025-12-08 17:04:48 -0500

    regenerate stubs

--- src/indikator/_rsi_numba.pyi
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from numpy.typing import NDArray
-
-def compute_rsi_numba(
-  prices: NDArray[np.float64],
-  window: int,
-  epsilon: float = ...,
-) -> NDArray[np.float64]: ...
