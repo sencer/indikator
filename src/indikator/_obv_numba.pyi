5594b20e549932027a8e93346fc7371b3c8730ff
Sencer S. <sencer@nospam.com>
2025-12-08 17:04:48 -0500

    regenerate stubs

--- src/indikator/_obv_numba.pyi
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from numpy.typing import NDArray
-
-def compute_obv_numba(
-  closes: NDArray[np.float64],
-  volumes: NDArray[np.float64],
-) -> NDArray[np.float64]: ...
--- src/indikator/_slope_numba.pyi
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from numpy.typing import NDArray
-
-def compute_slope_numba(
-  closes: NDArray[np.float64],
-  window: int,
-) -> NDArray[np.float64]: ...
